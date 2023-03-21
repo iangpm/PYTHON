8248dd43d70c041072efa38487794a53f9983f98
Ian Guedes <114230677+iangpm@users.noreply.github.com>
2023-03-20 21:23:07 -0300

    Update Ex02.py

--- Ex02.py
+++ Ex02.py
@@ -3,8 +3,8 @@ nota1 = float(input('Insira a primeira nota: '))
 nota2 = float(input('Insira a segunda nota: '))
 media = (nota1 + nota2) / 2
 
+print('Sua média foi {}'.format(media))
 if media >= 6:
     print('Você foi aprovado!')
 else:
     print('Você não foi aprovado!')
-    print(media)
